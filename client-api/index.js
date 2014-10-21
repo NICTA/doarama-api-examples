d78de63118c373e132c4aed1a18dc30600f22a3e
Chris Cooper <chris.cooper@nicta.com.au>
2014-10-21 15:46:47 +1100

    demonstrate early hiding of UI elements

--- client-api/index.js
+++ client-api/index.js
@@ -1,34 +1,49 @@
 var apiDomain = 'http://api.doarama.com';
-var apiReady = false;
+
+var playerLoaded = new $.Deferred();
+var playerApiReady = new $.Deferred();
+var playerReady = new $.Deferred();
 var player;
 
-// Only allow posting messages once API is ready
 function wrapPost(method, value) {
   var msg = {
     "method" : method,
     "value" : value
   };
-  if (typeof player !== 'undefined' && apiReady) {
+  // Only allow posting messages once API is ready
+  $.when(playerLoaded, playerApiReady).then(function(player) {
+    console.log("posting: " + JSON.stringify(msg));
     player.postMessage(msg, apiDomain);
-  } else {
-    alert("Doarama Client API not ready yet!");
-  }
+  });
 }
 
 document.getElementById('doarama-iframe').onload = function() {
-
   // Access the iframe's content window
   player = document.getElementById('doarama-iframe').contentWindow;
 
-  // Receive messages from the API
-  function onMessageReceived(e) {
-    if (e.data.method === 'ready') {
-      apiReady = true;
-      console.log('API is ready!');
-    } else {
-      console.log('client received: ' + JSON.stringify(e.data));
-    }
-  }
-
-  window.addEventListener('message', onMessageReceived, false);
+  console.log('iframe has loaded!');
+  playerLoaded.resolve(player);
 };
+
+// Receive messages from the API
+function onMessageReceived(e) {
+
+  if (e.data.method === 'ready') {
+    console.log('Player is ready and all tracks loaded!');
+    playerReady.resolve();
+
+  } else if (e.data.method === 'api-ready') {
+    console.log('API is ready!');
+    playerApiReady.resolve();
+
+  } else {
+    console.log('client received: ' + JSON.stringify(e.data));
+  }
+}
+
+// We can do some initial UI visibility configuration before tracks have loaded
+// NOTE: Only visibility configuration is to be done before playerReady has resolved
+wrapPost('setChartVisibility', false);
+wrapPost('setStatsVisibility', false);
+
+window.addEventListener('message', onMessageReceived, false);
